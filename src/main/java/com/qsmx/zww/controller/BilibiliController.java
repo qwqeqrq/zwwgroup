e8cb9eb7f29487f49c87688b3e55ee9e6bce13e3
1374559379 <1374559379@qq.com>
2019-03-29 09:32:04 +0800

    删除多余的jar

--- src/main/java/com/qsmx/zww/controller/BilibiliController.java
+++ src/main/java/com/qsmx/zww/controller/BilibiliController.java
@@ -1,12 +1,10 @@
 package com.qsmx.zww.controller;
 
-import ch.qos.logback.classic.spi.STEUtil;
+
 import com.qsmx.zww.mapper.BilibiliMapper;
 import com.qsmx.zww.servcie.BilibiliService;
 import com.qsmx.zww.uitil.JsonUtil;
-import javafx.scene.chart.ValueAxis;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
