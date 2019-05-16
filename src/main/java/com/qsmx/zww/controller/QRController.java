b29077a624641868e9f6648ad37155871289a9d7
1374559379 <1374559379@qq.com>
2019-05-16 10:49:20 +0800

    将数据用输出流输出

--- src/main/java/com/qsmx/zww/controller/QRController.java
+++ src/main/java/com/qsmx/zww/controller/QRController.java
@@ -5,8 +5,11 @@ import com.google.zxing.common.BitMatrix;
 import com.qsmx.zww.uitil.QRCodeUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import javax.servlet.http.HttpServletResponse;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 
 /**
  * Created by zww on 2019-04-12.
@@ -19,8 +22,10 @@ public class QRController {
     @RequestMapping(value = "QRcode")
     public void getQRcdoe(HttpServletResponse httpServletResponse) {
         try {
-            BitMatrix QRimage = QRCodeUtil.generateQRCodeStream("https://www.csdn.net/", httpServletResponse);
+            BitMatrix QRimage = QRCodeUtil.generateQRCodeStream("https://www.baidu.com", httpServletResponse);
+            OutputStream outputStream = new FileOutputStream("D:\\1.jpg");//用输入流创建一个路径，一会吧二维码数据流写入该文件
             MatrixToImageWriter.writeToStream(QRimage, "jpg", httpServletResponse.getOutputStream());
+            MatrixToImageWriter.writeToStream(QRimage, "jpg", outputStream);
         } catch (IOException e) {
             e.printStackTrace();
         }
