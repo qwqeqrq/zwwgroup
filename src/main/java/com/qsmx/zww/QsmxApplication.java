eb584821e53eb09b8bd7b9e6e57976d9965c7d93
ZHANGWEI <1374559379>
2019-09-05 20:32:49 +0800

    springboot 内嵌Tomcat 新增HTTP请求进行非法字符过滤 现在放行部分字符

--- src/main/java/com/qsmx/zww/QsmxApplication.java
+++ src/main/java/com/qsmx/zww/QsmxApplication.java
@@ -2,7 +2,11 @@ package com.qsmx.zww;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -17,5 +21,16 @@ public class QsmxApplication {
         //使用SpringApplication.run启动springboot项目
         //也可打成war包部署在Tomcat服务器，需要在这实现Sevletinint类,去掉pom相关tomcat注解、以后在体验 不推荐
         SpringApplication.run(QsmxApplication.class);
+    }
+
+    /**
+     * springboot 内嵌Tomcat 新增HTTP请求进行非法字符过滤 现在放行部分字符
+     * @return
+     */
+    @Bean
+    public ConfigurableServletWebServerFactory webServerFactory() {
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+        factory.addConnectorCustomizers((TomcatConnectorCustomizer) connector -> connector.setProperty("relaxedQueryChars", "|{}[]\\"));
+        return factory;
     }
 }
